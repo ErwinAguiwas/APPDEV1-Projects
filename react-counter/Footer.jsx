9db8b258395bc25d6081692a7ebf420d3cd4bb1a
Erwin G. Aguiwas <20237098@s.ubaguio.edu>
2024-12-05 20:45:42 +0800

    Delete react-counter directory

--- react-counter/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer(){
-    return(
-        <footer>
-            <hr />
-            <p>My Counter Website || Written by: Erwin G. Aguiwas</p>
-        </footer>
-    )
-}
