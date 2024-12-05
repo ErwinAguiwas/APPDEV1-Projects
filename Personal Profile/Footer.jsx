44b504a35bc3fc564c45f0de11517abd8192036d
Erwin G. Aguiwas <20237098@s.ubaguio.edu>
2024-12-05 20:45:03 +0800

    Delete Personal Profile directory

--- Personal Profile/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>&copy; {new Date().getFullYear()} Erwin Aguiwas  -^-^-</p>
-    </footer>
-  );
-}
-
-export default Footer;
