315b280a20c152f940b66418065e1bdbe3d2ebbd
Erwin G. Aguiwas <20237098@s.ubaguio.edu>
2024-12-05 20:45:49 +0800

    Delete updating-arrays-react directory

--- updating-arrays-react/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer(){
-    return(
-        <footer>
-            <hr />
-            <p>&copy; {new Date().getFullYear() }My Games Website || Written by: Erwin G. Aguiwas</p>
-        </footer>
-    )
-}
